82bd224307a4a0651a27a08916eb769e018a62ac
girishgupta211 <girishgargdce@gmail.com>
2017-07-27 18:26:27 +0530

    changes in API

--- server/api/api.controller.js
+++ server/api/api.controller.js
@@ -7,16 +7,17 @@ var options = {
   //host : absolutePath,	
   host: ' http://localhost:8545',
   ipc : false,
-  personal: true, 
+  personal: true,
   admin: false,
   debug: false
 };
-
+var adminAddress = "0xdecaaf96d20af1819faedb11c7912d2671189855";
+var patientContractAddress = '0x44b348d94e99e50bea5b0a478099dffa7edc876e';
 var web3 = web3_extended.create(options);
 /*Admin Contract*/
 var adminAbiString = '[{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"patients","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"records","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"patientsAddr","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"id","type":"address"},{"name":"day","type":"uint256"},{"name":"data","type":"string"}],"name":"addRecords","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getFirstPatietAddress","outputs":[{"name":"addr","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"},{"name":"p_address","type":"string"},{"name":"dob","type":"uint256"},{"name":"blood_grp","type":"string"},{"name":"phnum","type":"string"}],"name":"registerPatient","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"Admin","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"address"},{"indexed":false,"name":"desc","type":"string"}],"name":"log","type":"event"}]';
 var adminAbi = JSON.parse(adminAbiString);
-var adminContract = web3.eth.contract(adminAbi).at("0xdc86a31bb3206d8df02db50f7c021a4d0ed6eb06");
+var adminContract = web3.eth.contract(adminAbi).at(adminAddress);
 var patientabi = JSON.parse('[{"constant":true,"inputs":[],"name":"getName","outputs":[{"name":"name","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getaddress","outputs":[{"name":"add","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"bpm","type":"uint256"},{"name":"bp","type":"uint256"},{"name":"spo2","type":"string"}],"name":"addHealthData","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getbloodgrp","outputs":[{"name":"blood_grp","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"p","outputs":[{"name":"name","type":"string"},{"name":"p_address","type":"string"},{"name":"dob","type":"uint256"},{"name":"blood_grp","type":"string"},{"name":"phnum","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getdob","outputs":[{"name":"dob","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getphnum","outputs":[{"name":"phnum","type":"string"}],"payable":false,"type":"function"},{"inputs":[{"name":"name","type":"string"},{"name":"p_address","type":"string"},{"name":"dob","type":"uint256"},{"name":"blood_grp","type":"string"},{"name":"phnum","type":"string"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"bpm","type":"uint256"},{"indexed":false,"name":"bp","type":"uint256"},{"indexed":false,"name":"spo2","type":"string"}],"name":"logEvent","type":"event"}]');        
 
 
@@ -26,53 +27,69 @@ exports.registerPatient = function(req, res){
     var dob = (req.body.dob);
     var bloodGroup = (req.body.bloodGroup);
     var mobileNo = (req.body.mobileNo);
-    var from = req.body.from;
+    var from = web3.eth.accounts[0]; //req.body.from;
     var txHash = adminContract.registerPatient.sendTransaction(name, address, dob, bloodGroup, mobileNo, {from:from, gas:697283});        
     var patientAddress = adminContract.getFirstPatietAddress();
     // Save patients to db
     savePatient(txHash,patientAddress,name,address,dob,bloodGroup,mobileNo);
-    return res.json({"success":"true","data":{'txHash': txHash, 'patientsAddress':patientAddress}});
+    return res.json({"success":"true",'txHash': txHash, 'patientsAddress':patientAddress});
 }
 
 exports.getPatinetDetails = function(req, res){
 	var patientId = req.body.patientId;
+    // console.log(patientId);
+
 	var patientContract = web3.eth.contract(patientabi).at(patientId);
+
 	var name = patientContract.getName();
     var dob = patientContract.getdob().toNumber();    
     var address = (patientContract.getaddress());
     var bloodGroup = (patientContract.getbloodgrp());
     var mobileNo = patientContract.getphnum();
-    return res.json({"success":"true","data":{'name': name, 'dob':dob, 'address':address , 'bloodGroup':bloodGroup, 'mobileNo':mobileNo}});
+    // console.log(mobileNo);
+    return res.json({"success":"true",'name': name, 'dob':dob, 'address':address , 'bloodGroup':bloodGroup, 'mobileNo':mobileNo });
 }
 
 exports.getTransactionReceipt = function(req, res){
 	var txHash = req.body.txHash;
-	var txReceipt = web3.eth.getTransactionReceipt(txhash);
+    // console.log(txHash);
+	var txReceipt = web3.eth.getTransactionReceipt(txHash);
+    // console.log(txReceipt)
 	var txhash = txReceipt.transactionHash;
     var txIndex = txReceipt.transactionIndex;
     var blockHash = txReceipt.blockHash;
     var blockNumber = txReceipt.blockNumber;
     var gasUsed = txReceipt.gasUsed;
     var cumulativeGasUsed = txReceipt.cumulativeGasUsed;
-    return res.json({"success":"true","data":{'txHash': txHash, 'txIndex':txIndex, 'blockHash': blockHash,  'blockNumber':blockNumber, 'gasUsed':gasUsed, 'cumulativeGasUsed':cumulativeGasUsed}});      
+
+    return res.json({"success":"true", 'txHash': txHash, 'txIndex':txIndex, 'blockHash': blockHash,  'blockNumber':blockNumber, 'gasUsed':gasUsed, 'cumulativeGasUsed':cumulativeGasUsed });
 }
 
 exports.logData = function(req, resp){
 	if($scope.startLoggingVar){
         counter++;
-        adminContract.addRecords.sendTransaction('0xc5f74b4d70f401043376aaa1b8c2edde32848deb',counter, "Hello",{from:web3.eth.accounts[0], gas:2100000});
+        // patient health data log on blockchain. so add patient address here
+        adminContract.addRecords.sendTransaction(patientContractAddress,counter, "Hello",{from:web3.eth.accounts[0], gas:2100000});
     }
 }
 
 exports.addPrescription = function(req, resp){	
 	var patientId = req.body.patientId;
-	var prescription = req.body.prescription;
+    var prescription = { medicineName : req.body.medicineName , timesADay : req.body.timesADay 
+        , fromDate : req.body.fromDate
+        , tillDate : req.body.tillDate
+        , doctorId : req.body.doctorId 
+         };
+	// var prescription = req.body.prescription; 
 	PatientService.addPrescription(patientId, prescription, function(resp1){
 		if(!resp1.error){
-			console.log("Resp from db : "+JSON.stringify(resp1));
+            // debugger;
+			// console.log("Resp from db : "+JSON.stringify(resp1));
             //create a calander for each prescription
+            console.log(prescription);
             for(var i=0; i<prescription.length ; i++){
                 var cp = prescription[i];
+                // debugger;
                 addSchedule(patientId,cp.medicineName,cp.fromDate,cp.tillDate,cp.timesADay);
             }
             // save logs
@@ -89,6 +106,7 @@ exports.addPrescription = function(req, resp){
 function addSchedule(patientId,medicineName,fromDate,tillDate,timesADay){
     // create calander will take params like id, tillDate, fromdate, medicineName, status
     PatientService.addSchedule(patientId,medicineName,fromDate,tillDate, timesADay,function(resp1){
+        console.log(resp1);
         if(!resp1.error){
             console.log("Resp from db : "+JSON.stringify(resp1));
             //create a calander for each prescriptionId
@@ -129,11 +147,11 @@ exports.getPrescriptionLogs = function(req, res){
     PatientService.getPrescriptionLog(patientId, function(resp1){
         if(!resp1.success){
             console.log("Error occured in getting prescription history");
-            return res.json({"success":"false","message":"Error occured in getting prescription history"});
+            return res.json({"message":"Error occured in getting prescription history"});
         }
         else{
             console.log("succesfully recieved prescription history");
-            return res.json({"success":"true","data":[{"result":resp1}]});
+            return res.json({"result":resp1});
         }
     })
 }
@@ -181,6 +199,7 @@ exports.countPatients = function (req, res){
 // saving transaction details to database.
 function savePatient(txId,patientId,name,address,dob,bloodGroup,mobileNo){
 	PatientService.savePatient(txId,patientId,name,address,dob,bloodGroup,mobileNo, function(resp){
+
 		if(!resp.error){
 			console.log("Resp from db : "+JSON.stringify(resp));
 		}
@@ -189,6 +208,3 @@ function savePatient(txId,patientId,name,address,dob,bloodGroup,mobileNo){
 		}
 	});
 } // save Patient ends here
-
-
-
